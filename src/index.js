63d1abb562b380ac4b98c0746ac9422ba3dd067a
Arthorius-Pen <utherpendragon@tutanota.com>
2022-08-11 22:02:49 -0300

    removed App import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import NavBar from './view/Navbar.js'
 import Produtos from './view/Produtos.js'
@@ -9,7 +8,6 @@ import Produtos from './view/Produtos.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <NavBar />
     <Produtos />
   </React.StrictMode>
